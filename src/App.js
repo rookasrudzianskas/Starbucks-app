77195503ead9f32f7512cd1508a03a0b13ea68a3
Rokas <rookas.rudzenskas@gmail.com>
2021-06-25 17:36:05 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>👇 Let's build the starbucks ☕ 👇</h1>
+      <Header />
     </div>
   );
 }

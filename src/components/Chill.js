f654db5e3185e37f1e90aa7c87f7f50d0101f5c6
Rokas <rookas.rudzenskas@gmail.com>
2021-06-26 10:49:12 +0300

    Rokas

--- src/components/Chill.js
+++ src/components/Chill.js
@@ -5,13 +5,16 @@ const Chill = () => {
     return (
         <div className="chill">
             <div className="chill__leftRight__section">
-
                 <div className="chill__image">
                     <img src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-72037.jpg" alt=""/>
                 </div>
+
                 <div className="chill__text">
                     <h1>DELIGHTFULLY CHILL</h1>
-                    <h3>Refresh your day with the bright, tropical flavors of our Star Drink and Kiwi Starfruit Starbucks Refreshers® beverage.</h3>
+                    <h3>Refresh your day with the bright,
+                        tropical flavors of our Star
+                        Drink and Kiwi Starfruit Starbucks
+                        Refreshers® beverage.</h3>
 
                     <div className="cta">
                         Order iced beverage

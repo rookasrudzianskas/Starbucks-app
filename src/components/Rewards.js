e29a1b9bcdfeb6cabeea5de9d0fc2d2f751bb195
Rokas <rookas.rudzenskas@gmail.com>
2021-06-26 10:03:33 +0300

    Rokas

---
+++ src/components/Rewards.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./styles/Rewards.css";
+
+const Rewards = () => {
+    return (
+        <div className="rewards">
+            <div className="rewards__leftRight__section">
+
+            </div>
+        </div>
+    );
+};
+
+export default Rewards;

b7108b9461fdaeeaa5b31ccbe33028e604a5f1b9
Rokas <rookas.rudzenskas@gmail.com>
2021-06-26 08:42:41 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,20 @@ const Header = () => {
         <div className="header">
             <div className="header__container">
                     <div className="header__logo">
-                        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png" alt=""/>
+                        <div className="header__leftNav">
+                            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png" alt=""/>
+                            <div className="header__nav">
+                                <ul>
+                                    <li>Menu</li>
+                                    <li>Rewards</li>
+                                    <li>Gift Cards</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div className="header__rightNav">
+
+                        </div>
                     </div>
             </div>
         </div>

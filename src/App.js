3546a13e285fc0d9a69256eab37889bb02a0b3ae
Rokas <rookas.rudzenskas@gmail.com>
2021-06-26 09:47:26 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./components/Header";
+import Announcment from "./components/Announcment";
 
 function App() {
   return (
     <div className="app">
       <Header />
+        <Announcment />
     </div>
   );
 }
---
+++ src/components/Announcment.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./styles/Announcment.css";
+
+const Announcment = () => {
+    return (
+        <div className="announcement">
+            <div className="announcment__mainContainer">
+                <div className="div announcements">
+                    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio excepturi facere quam sequi tempore. Excepturi harum in modi quasi voluptates?</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Announcment;

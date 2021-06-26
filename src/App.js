26f8d45332cc494f67dc9ead433b2fd2b07a8f66
Rokas <rookas.rudzenskas@gmail.com>
2021-06-26 10:06:52 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from "./components/Header";
 import Announcment from "./components/Announcment";
+import Rewards from "./components/Rewards";
 
 function App() {
   return (
     <div className="app">
       <Header />
         <Announcment />
+        <Rewards />
     </div>
   );
 }
--- src/components/Rewards.js
+++ src/components/Rewards.js
@@ -5,7 +5,18 @@ const Rewards = () => {
     return (
         <div className="rewards">
             <div className="rewards__leftRight__section">
+                <div className="rewards__text">
+                    <h1>FREE COFFE IS A TAP AWAY</h1>
+                    <h3>Join now to start earning Rewards.*</h3>
 
+                    <div className="cta">
+                        Join now
+                    </div>
+                </div>
+
+                <div className="rewards__image">
+                    <img src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-71056.png" alt=""/>
+                </div>
             </div>
         </div>
     );

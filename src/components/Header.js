876bd19d8fb683a0bae2ec132b5ad24d86808be2
Rokas <rookas.rudzenskas@gmail.com>
2021-06-26 08:30:39 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ import "./styles/Header.css";
 const Header = () => {
     return (
         <div className="header">
-
+                <div className="header__logo">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png" alt=""/>
+                </div>
         </div>
     );
 };

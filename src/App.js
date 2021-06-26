92bf15f9eee44d5052d20273dac5a92763da77f4
Rokas <rookas.rudzenskas@gmail.com>
2021-06-26 11:55:06 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Announcment from "./components/Announcment";
 import Rewards from "./components/Rewards";
 import Chill from "./components/Chill";
+import TopBottomSection from "./components/Top-BottomSection";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Announcment />
         <Rewards />
       <Chill />
+        <TopBottomSection />
     </div>
   );
 }
---
+++ src/components/Top-BottomSection.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import "./styles/topBottomSection.css";
+
+const TopBottomSection = () => {
+    return (
+        <div className="topBottomSection">
+            <div className="topBottomSection__topBottom-section">
+                <div className="topBottomSection__section__container">
+
+                    <div className="topBottomSection__image">
+                        <img src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-72037.jpg" alt=""/>
+                    </div>
+
+                    <div className="topBottomSection__text">
+                        <h1>DELIGHTFULLY CHILL</h1>
+                        <h3>Refresh your day with the bright,
+                            tropical flavors of our Star
+                            Drink and Kiwi Starfruit Starbucks
+                            Refreshers® beverage.</h3>
+
+                        <div className="cta">
+                            Order iced beverage
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="topBottomSection__topBottom-section">
+                <div className="topBottomSection__section__container">
+
+                    <div className="topBottomSection__image">
+                        <img src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-72037.jpg" alt=""/>
+                    </div>
+
+                    <div className="topBottomSection__text">
+                        <h1>DELIGHTFULLY CHILL</h1>
+                        <h3>Refresh your day with the bright,
+                            tropical flavors of our Star
+                            Drink and Kiwi Starfruit Starbucks
+                            Refreshers® beverage.</h3>
+
+                        <div className="cta">
+                            Order iced beverage
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default TopBottomSection;

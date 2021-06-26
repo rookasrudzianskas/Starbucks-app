48bc0c9ce12889e36d729a55d959d299b215a544
Rokas <rookas.rudzenskas@gmail.com>
2021-06-26 10:39:00 +0300

    Rokas

--- src/components/Chill.js
+++ src/components/Chill.js
@@ -10,11 +10,11 @@ const Chill = () => {
                     <img src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-72037.jpg" alt=""/>
                 </div>
                 <div className="chill__text">
-                    <h1>FREE COFFE IS A TAP AWAY</h1>
-                    <h3>Join now to start earning Rewards.*</h3>
+                    <h1>DELIGHTFULLY CHILL</h1>
+                    <h3>Refresh your day with the bright, tropical flavors of our Star Drink and Kiwi Starfruit Starbucks Refreshers® beverage.</h3>
 
                     <div className="cta">
-                        Join now
+                        Order iced beverage
                     </div>
                 </div>
             </div>
